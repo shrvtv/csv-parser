f4dce8eb5f84546425511c9dc886e4fc632fb6e3
Artem Sharovatov <git@sharovatov.me>
2026-03-27 22:09:29 +0300

    Add more arguments to the CLI

--- main.py
+++ main.py
@@ -10,10 +10,22 @@ def main():
         "--files",
         nargs="+",
         help="specify the files to parse",
+        required=True,
     )
     parser.add_argument(
         "--report",
         help="specify the report filename",
+    )
+    parser.add_argument(
+        "-f", "--formula",
+        help="select the math formula to use",
+        choices=["median"],
+        default="median",
+    )
+    parser.add_argument(
+        "-c", "--column",
+        help="specify the column name",
+        default="coffee_spent"
     )
 
     args = parser.parse_args()
