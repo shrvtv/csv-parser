d40ec8448c2bb488478bc8c8e5c2aa66c5d2cf5c
Artem Sharovatov <git@sharovatov.me>
2026-03-27 18:05:16 +0300

    Add required parser arguments

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+#!/usr/bin/env python3
+import argparse
+
+
 def main():
-    print("Hello from csv-parser!")
+    parser = argparse.ArgumentParser(
+        description="Parses CSV files and forms a report"
+    )
+    parser.add_argument(
+        "--files",
+        nargs="+",
+        help="specify the files to parse",
+    )
+    parser.add_argument(
+        "--report",
+        help="specify the report filename",
+    )
+
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
