81710651a516dea3eb0593315fb323e3f52465ca
Artem Sharovatov <git@sharovatov.me>
2026-03-28 12:06:38 +0300

    Add reading and merging datasets functionality

--- parsers.py
+++ parsers.py
@@ -1,9 +1,19 @@
 import csv, itertools
 
 
+def read_csv(filename: str) -> list[dict[str, str]]:
+    with open(filename) as csvfile:
+        return list(csv.DictReader(csvfile))
+
+
+def merge_csv_files(filenames: list[str]) -> list[dict[str, str]]:
+    return list(itertools.chain.from_iterable(map(read_csv, filenames)))
+
+
 def csv_parser(
         filenames: list[str],
         formula: str,
         column: str
 ) -> list[dict[str, str]]:
-    pass
+    dataset = merge_csv_files(filenames)
+    return dataset
