fd7289ae0e1d51e745a4c1580ae9b346afe4a8fc
Artem Sharovatov <git@sharovatov.me>
2026-03-29 11:19:33 +0300

    Add test for reading files with parser

--- tests/test_main.py
+++ tests/test_main.py
@@ -43,3 +43,17 @@ def test_cli_output():
     )
 
     assert result.stdout == Path("tests/fixtures/expected.txt").read_text()
+
+
+def test_read(csv_parser, math_csv):
+    data = csv_parser.read(math_csv)
+    assert len(data) == 45
+    assert data[0] == {
+        "student": "Алексей Смирнов",
+        "date": "2024-06-01",
+        "coffee_spent": "450",
+        "sleep_hours": "4.5",
+        "study_hours": "12",
+        "mood": "норм",
+        "exam": "Математика",
+    }
